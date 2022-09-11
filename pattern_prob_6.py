aebadb2a9271533964dd647638f5f27523627548
al-ruhanyat <alruhanyat994@gmail.com>
2022-09-11 20:10:09 +0600

    6th from here

---
+++ pattern_prob_6.py
@@ -0,0 +1,10 @@
+'''printing a hill pattern'''
+n=int(input("Enter the number you like :"))
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end="")
+    for j in range(i):#for decreasing a coloumn
+        print("*",end="")
+    for j in range(i+1):
+        print("*",end="")
+    print() 
